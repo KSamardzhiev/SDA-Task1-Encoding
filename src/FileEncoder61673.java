022ae4cfe3d5d139e7199b2d75b3be8d189614b8
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2014-11-16 19:42:04 +0200

    test github

---
+++ src/FileEncoder61673.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+
+public class FileEncoder61673 {
+
+		public void encode(String sourceFile, String destinationFile, LinkedList<Character> key)
+		{
+			
+		}
+		public void decode(String sourceFile, String destinationFile, LinkedList<Character> key)
+		{
+			
+		}
+}
---
+++ src/Testing.java
@@ -0,0 +1,10 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		FileEncoder61673 a = new FileEncoder61673();
+
+	}
+
+}
